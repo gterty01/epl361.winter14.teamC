21e4460f68a8eb19a480d37c5b951aecf99bc8c7
etingi01 <etingi01@cs.ucy.ac.cy>
2015-02-28 20:17:34 +0200

    Mikres allages sta index.html kai register.html kai sto registration.php

--- temp1.4/registration.php
+++ temp1.4/registration.php
@@ -24,9 +24,72 @@ if (!$conn) {
     echo "Connection faild";
 }
 echo "Connected successfully";
+if($_POST["name"] && $_POST["surname"] && $_POST["mail"] && $_POST["passwd"] && $_POST['passwdCon'] && $_POST['birth'] && $_POST['city'] && $_POST['postalCode'] && $_POST['address1'])
+{
+
+$email=$_POST['mail'];
+
+$query = mysql_query("SELECT * FROM USERS_FM WHERE Email='$email'");
+
+if(mysql_num_rows($query) != 0)
+{
+echo "Username already exists";
+}
+else{
+$name = $_POST["name"];
+$surname = $_POST['surname'];
+$passwd = $_POST['passwd'];
+$passwdCon = $_POST['passwdCon'];
+$birth = $_POST['birth'];
+$city = $_POST['city'];
+$postalCode = $_POST['postalCode'];
+$address1 = $_POST['address1'];
+
+$filo=$_POST['filo'];
+$address2=$_POST['address2'];
+$country=$_POST['country'];
+$perioxi=$_POST['poli'];
+$codeCi=$_POST['codeC1'];
+$codeC2=$_POST['codeC2'];
+$tel1=$_POST['tel1'];
+$tel2=$_POST['tel2'];
+
+if ($_POST['filo']=="Φύλο"){
+ $filo = "";
+}else{ $filo = $_POST['filo']}
+
+if ($_POST['address2']=="Διεύθυνση 2"){
+ $address2 = "";
+}else{ $address2 = $_POST['filo']}
+
+if ($_POST['country']=="Διάλεξε τη Χώρα σου"){
+ $country = "";
+}else{ $country = $_POST['country']}
+
+if ($_POST['poli']=="Περιοχή"){
+ $poli = "";
+}else{ $poli = $_POST['poli']}
 
 
-echo "something here"
+$sql="insert into USERS_FM (name,email,password)values('$_GET[regname]','$_GET[regemail]','$_GET[regpass1]')";
+
+
+
+
+
+}
+
+
+
+
+
+
+}
+
+
+
+
+
 
 ?>
 
