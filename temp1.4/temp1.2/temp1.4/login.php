bf7ca19534c5a3d9782b4d095008de009c912ac5
rlouka01@cs.ucy.ac.cy <rlouka01@cs.ucy.ac.cy>
2015-03-08 01:42:27 +0200

    allages sto login

--- temp1.4/temp1.2/temp1.4/login.php
+++ temp1.4/temp1.2/temp1.4/login.php
@@ -1,44 +1,48 @@
-<html>
-<head>
-<title>Login</title>
-</head>
-<body>
 <?php
+	session_start();
+	$error = ''; 
+	$servername = "localhost";
+	$username = "cyfoodmuseum";
+	$password = "9m8ESxZD";
+	$dbname = "cyfoodmuseum";
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	    echo "Connection faild";
+	}
+	echo "Connected successfully";
 
-$servername = "localhost";
-$username = "cyfoodmuseum";
-$password = "9m8ESxZD";
-$dbname = "cyfoodmuseum ";
-
-$conn = mysql_connect($servername, $username, $password) or die ( "Could not connect: " . mysql_error() );
-$database = mysql_select_db($dbname, $conn) or die ("Could not select database: " . mysql_error() );
-
-$email = $_POST['email'];
-print ($email);
-$password = $_POST['password'];
-print ($password);
-
-
-
-//$sql = "SELECT * FROM 'USER_FM' WHERE Email = '" . $email . "' and Password = '" . $password "' ";
-$sql ="SELECT * FROM `USER_FM` WHERE Email ='$email' AND Password ='$password'";
-$result=mysql_query($sql);
-$count=mysql_num_rows($result);
-
-    if ($count == 1){
-         header("location:index.html"); /* Redirect the browser */
-    }  
-	else {
-       echo "Λάθος email ή κωδικός! Προσπάθησε ξανά ";  
-       //header('Location:login.php');
-
-        }
-
-
-exit();
-
+	parse_url(file_get_contents("php://input"), $_POST);
+	print_r($_POST); 
+	
+	$email = $_POST['mail'];
+	print ($email);
+	$password = $_POST['password'];
+	print ($password);
+	if ($_POST['mail'] && $_POST['password']){
+	
+	//$sql = "SELECT * FROM 'USER_FM' WHERE Email = '" . $email . "' and Password = '" . $password "' ";
+	$sql ="SELECT * FROM `USER_FM` WHERE Email ='$email' AND Password ='$password'";
+	$result=$conn->query($sql);
+	
+	   if($result->num_rows == 1)
+		{
+			$_SESSION['login_user']=$email;
+	        header("location:index.html"); /* Redirect the browser */
+	    }  
+		else {
+			$error = "Το email ή ο κωδικός που έδωσες είναι λάθος!";
+			header("Location:login.html");
+		}
+	}
+	else
+	{
+		$error = "Το email ή ο κωδικός που έδωσες είναι λάθος!";
+		header("Location:login.html");
+	}
+	
+	//exit();
 
 ?>
-
-</body>
-</html>
